4c44fe5174024783d0a2f2590e11736312dc76a1
boissonnat <alexis.boissonnat@cstb.fr>
2011-09-27 11:31:20 +0200

    Init a new page for About

---
+++ src/main/java/org/alx/action/AboutActionBean.java
@@ -0,0 +1,17 @@
+package org.alx.action;
+
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
+
+@UrlBinding("/About.htm")
+public class AboutActionBean extends BaseActionBean {
+
+    private static final String VIEW = "/WEB-INF/jsp/about.jsp";
+
+    @DefaultHandler
+    public Resolution view() {
+        return new ForwardResolution(VIEW);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/about.jsp
@@ -0,0 +1,9 @@
+<%@ include file="/WEB-INF/jsp/common/taglibs.jsp" %>
+
+<s:layout-render name="/WEB-INF/jsp/common/layout.jsp" title="About">
+  <s:layout-component name="body">
+    <p>
+        About this project !
+    </p>
+  </s:layout-component>
+</s:layout-render>
