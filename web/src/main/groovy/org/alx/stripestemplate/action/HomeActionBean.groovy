12f1312451f1314a46284a3055b1235837eeac53
boissonnat <alexis.boissonnat@cstb.fr>
2011-09-28 14:35:19 +0200

    Add specific home for User and Admin role

--- web/src/main/groovy/org/alx/stripestemplate/action/HomeActionBean.groovy
+++ web/src/main/groovy/org/alx/stripestemplate/action/HomeActionBean.groovy
@@ -4,6 +4,8 @@ import net.sourceforge.stripes.action.DefaultHandler
 import net.sourceforge.stripes.action.ForwardResolution
 import net.sourceforge.stripes.action.Resolution
 import net.sourceforge.stripes.action.UrlBinding
+import org.alx.stripestemplate.model.User
+import org.alx.stripestemplate.util.AppConstants
 
 /**
  * @author Alexis Boissonnat - alexis.boissonnat 'at' gmail.com
@@ -11,11 +13,19 @@ import net.sourceforge.stripes.action.UrlBinding
 @UrlBinding('/Home.htm')
 class HomeActionBean extends BaseActionBean {
 
-    private static final String VIEW = "/WEB-INF/jsp/home.jsp"
+    private static final String GUEST_HOME = "/WEB-INF/jsp/home.jsp";
+    private static final String USER_HOME = "/WEB-INF/jsp/user/home.jsp";
+    private static final String ADMIN_HOME = "/WEB-INF/jsp/admin/home.jsp";
 
     @DefaultHandler
     Resolution view() {
-        return new ForwardResolution(VIEW)
+        User u = context.user
+        if (!u){
+            return new ForwardResolution(GUEST_HOME)
+        }else if (u.hasRole(AppConstants.ROLE_USER)){
+            return new ForwardResolution(USER_HOME)
+        }else {
+            return new ForwardResolution(ADMIN_HOME)
+        }
     }
-
 }
---
+++ web/src/main/webapp/WEB-INF/jsp/admin/home.jsp
@@ -0,0 +1,11 @@
+<%@ include file="/WEB-INF/jsp/common/taglibs.jsp" %>
+
+<fmt:message var="title" key="home.page.title"/>
+<s:layout-render name="/WEB-INF/jsp/common/layout.jsp" title="${title}">
+    <s:layout-component name="body">
+        <h1><fmt:message key="home.title"/></h1>
+        <p>
+            <fmt:message key="home.admin.content"/>
+        </p>
+    </s:layout-component>
+</s:layout-render>
