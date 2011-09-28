aa61a8fcb6050eff0460b7a2386c76f8c8179ebb
boissonnat <alexis.boissonnat@cstb.fr>
2011-09-28 11:27:40 +0200

    Prepare Stripes context to store and retrieve User

--- web/src/main/groovy/org/alx/stripestemplate/action/BaseActionBean.groovy
+++ web/src/main/groovy/org/alx/stripestemplate/action/BaseActionBean.groovy
@@ -2,12 +2,70 @@ package org.alx.stripestemplate.action
 
 import net.sourceforge.stripes.action.ActionBean
 import net.sourceforge.stripes.action.ActionBeanContext
+import org.alx.stripestemplate.stripes.ext.MyActionBeanContext
+import org.alx.stripestemplate.stripes.ext.MyLocalePicker
+import javax.servlet.http.HttpServletRequest
+import net.sourceforge.stripes.action.LocalizableMessage
+import org.alx.stripestemplate.persistence.HibernateStore
 
 /**
  * @author Alexis Boissonnat - alexis.boissonnat 'at' gmail.com
  */
 class BaseActionBean implements ActionBean {
 
-    ActionBeanContext context
+    private MyActionBeanContext context;
+
+    public MyActionBeanContext getContext() {
+        return context;
+    }
+    public void setContext(ActionBeanContext context) {
+        this.context = (MyActionBeanContext) context;
+    }
+
+    public String getLastUrl() {
+        HttpServletRequest req = getContext().getRequest();
+        StringBuilder sb = new StringBuilder();
+
+        // Start with the URI and the path
+        String uri = (String)
+            req.getAttribute("javax.servlet.forward.request_uri");
+        String path = (String)
+            req.getAttribute("javax.servlet.forward.path_info");
+        if (uri == null) {
+            uri = req.getRequestURI();
+            path = req.getPathInfo();
+        }
+        sb.append(uri);
+        if (path != null) { sb.append(path); }
+
+        // Now the request parameters
+        sb.append('?');
+        Map<String,String[]> map =
+            new HashMap<String,String[]>(req.getParameterMap());
+
+        // Remove previous locale parameter, if present.
+        map.remove(MyLocalePicker.LOCALE);
+
+        // Append the parameters to the URL
+        for (String key : map.keySet()) {
+            String[] values = map.get(key);
+            for (String value : values) {
+                sb.append(key).append('=').append(value).append('&');
+            }
+        }
+        // Remove the last '&'
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
+    }
+
+    protected LocalizableMessage getLocalizableMessage(String key, Object... parameters){
+        return new LocalizableMessage(
+            getClass().getName() + "." + key, parameters);
+    }
+
+    HibernateStore getStore(){
+        return getContext().getStore()
+    }
 
 }
--- web/src/main/java/org/alx/stripestemplate/stripes/ext/MyActionBeanContext.java
+++ web/src/main/java/org/alx/stripestemplate/stripes/ext/MyActionBeanContext.java
@@ -1,21 +1,59 @@
 package org.alx.stripestemplate.stripes.ext;
 
 import net.sourceforge.stripes.action.ActionBeanContext;
+import org.alx.stripestemplate.model.User;
 import org.alx.stripestemplate.persistence.HibernateStore;
 import org.alx.stripestemplate.stripes.noext.MyInitListener;
 import org.alx.stripestemplate.stripes.noext.MyInitListener;
 import org.alx.stripestemplate.stripes.noext.MyInitListener;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Alexis Boissonnat - alexis.boissonnat 'at' gmail.com
  */
 public class MyActionBeanContext extends ActionBeanContext {
 
-  protected void setCurrent(String key, Object value) {
-    getRequest().getSession().setAttribute(key, value);
-  }
+    private static final String USER = "user";
 
-  public HibernateStore getStore(){
-    return (HibernateStore) getServletContext().getAttribute(MyInitListener.CTX_STORE_KEY);
-  }
+    public User getUser() {
+        Integer userId = getCurrent(USER, null);
+        if (userId == null) { return null ; }
+        return (User) getStore().load(User.class, userId);
+    }
+
+    public void setUser(User user) {
+        if (user != null) {
+            setCurrent(USER, user.getId());
+        }
+        else {
+            setCurrent(USER, null);
+        }
+    }
+
+    public void logout() {
+        setUser(null);
+
+        HttpSession session = getRequest().getSession();
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+
+    protected <T> T getCurrent(String key, T defaultValue) {
+        T value = (T) getRequest().getSession().getAttribute(key);
+        if (value == null) {
+            value = defaultValue;
+            setCurrent(key, value);
+        }
+        return value;
+    }
+
+    protected void setCurrent(String key, Object value) {
+        getRequest().getSession().setAttribute(key, value);
+    }
+
+    public HibernateStore getStore(){
+        return (HibernateStore) getServletContext().getAttribute(MyInitListener.CTX_STORE_KEY);
+    }
 }
