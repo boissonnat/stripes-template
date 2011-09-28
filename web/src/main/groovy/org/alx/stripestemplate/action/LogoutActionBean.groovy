517a95f5f374b804ab5ae2fd9569003cf37d8667
boissonnat <alexis.boissonnat@cstb.fr>
2011-09-28 11:30:14 +0200

    Add a log out action bean

---
+++ web/src/main/groovy/org/alx/stripestemplate/action/LogoutActionBean.groovy
@@ -0,0 +1,21 @@
+package org.alx.stripestemplate.action
+
+import net.sourceforge.stripes.action.RedirectResolution
+import net.sourceforge.stripes.action.Resolution
+import net.sourceforge.stripes.action.UrlBinding
+import net.sourceforge.stripes.action.DefaultHandler
+
+/**
+ * @author Alexis Boissonnat - alexis.boissonnat 'at' gmail.com
+ */
+@UrlBinding('/Logout.htm')
+class LogoutActionBean extends BaseActionBean{
+
+    @DefaultHandler
+    public Resolution logout() {
+        context.logout()
+        return new RedirectResolution(HomeActionBean.class)
+
+    }
+
+}
